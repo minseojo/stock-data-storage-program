982683077ed1eb75a16a1d880d15ae07e55298d2
Yunsik-Choi <choiys0311@gmail.com>
2023-04-19 00:53:29 +0900

    refactor : 코드 리팩터링

--- stockSave/untitled/src/Stock.java
+++ stockSave/untitled/src/Stock.java
@@ -1,23 +1,36 @@
-class Stock {
-    private String name;
-    private String code;
+import java.util.Arrays;
+import java.util.List;
 
-    Stock(String name, String code) {
+public enum Stock {
+    EV_ADVANCED_MATERIALS("이브이첨단소재", "131400"),
+    CRYSTAL_NEW_MATERIAL("크리스탈신소재 ", "900250"),
+    POSCO_M_TECH("포스코엠텍 ", "009520"),
+    EID("이아이디 ", "093230"),
+    URBAN_LITHIUM("어반리튬 ", "073570"),
+    ECO_PRO("에코프로 ", "086520"),
+    MOBASE("모베이스 ", "101330"),
+    TSI("티에스아이 ", "277880 "),
+    DONGKUK_INDUSTRY("동국산업 ", "005160");
+
+    private final String name;
+    private final String code;
+
+    Stock(final String name, final String code) {
         this.name = name;
         this.code = code;
     }
 
+    public static List<Stock> sortedValues() {
+        Stock[] values = Stock.values();
+        Arrays.sort(values);
+        return List.of(values);
+    }
+
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public String getCode() {
-        return code;
-    }
-
-    @Override
-    public String toString() {
-        return "주식이름: " + name + "\n" +
-                "주식코드: " + code;
+        return this.code;
     }
 }

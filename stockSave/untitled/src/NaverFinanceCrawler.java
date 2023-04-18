cd407d52c6648548ac2c5131eb8ec8c24a0c25b1
minseojo <64322765+minseojo@users.noreply.github.com>
2023-04-19 01:22:45 +0900

    윤식님 리퀘스트 참고해서 리팩토링
    https://github.com/minseojo/stock-data-storage-program/pull/2

--- stockSave/untitled/src/NaverFinanceCrawler.java
+++ stockSave/untitled/src/NaverFinanceCrawler.java
@@ -6,110 +6,83 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class NaverFinanceCrawler {
+    private static final String FILE_PATH_FORMAT = "D:/stock/files/%s.txt";
+    private static final String STOCK_URL_FORMAT = "https://finance.naver.com/item/sise.naver?code=%s";
     private ArrayList<Stock> stocks = new ArrayList<>(); // 크롤링 할 주식들
-    private ArrayList<String> result = new ArrayList<>(); // 크롤링 결과
 
-    String fileName = "";
-    private FileWriter fw = null;
-    private PrintWriter out = null;
+    private static FileWriter fw = null;
+    private static PrintWriter out = null;
     NaverFinanceCrawler(ArrayList<Stock> stocks) {
         this.stocks = stocks;
     }
-    public void exe() {
-        try {
-            openFile();
-            crawling();
+    public static void exe(final List<Stock> stocks) {
+        try (
+                FileWriter fileWriter = new FileWriter(String.format(FILE_PATH_FORMAT, LocalDate.now()), false); // 파일 있으면, 처음부터 쓰기
+                PrintWriter printWriter = new PrintWriter(fileWriter, true)
+        ) {
+            printWriter.println(String.join("\n", crawling(stocks)));
             System.out.println("크롤링 성공");
         } catch (Exception e) {
             System.out.println("크롤링 실패: " + e);
-        } finally {
-            closeFile();
-        }
-    }
-    private void openFile() {
-        SimpleDateFormat today = new SimpleDateFormat("yyyyMMdd");
-        String date = today.format(new Date());
-        try {
-
-            fileName = "C:/Users/user/Desktop/stock-data-storage-program/files/" + date + ".txt";
-            fw = new FileWriter(fileName, false); // false = 파일 있으면 새로 쓰기
-            out = new PrintWriter(fw, true);
-        } catch (Exception e) {
-            System.out.println("파일 가져오기 실패: " + e);
         }
     }
 
-    private void closeFile() {
+    private static List<String> crawling(final List<Stock> stocks) {
+        return stocks.stream()
+                .map(NaverFinanceCrawler::toString)
+                .collect(Collectors.toList());
+    }
+
+    private static String toString(final Stock stock) {
         try {
-            if(out != null) {
-                out.close();
-            }
-            if(fw != null) {
-                fw.close();
-            }
+            Document doc = Jsoup.connect(String.format(STOCK_URL_FORMAT, stock.getCode())).get();
+            return changeStockToFilePrintFormat(stock);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("크롤링 실패 name:" + stock.getName() + " code: " + stock.getCode());
         }
-
     }
-    public ArrayList<String> crawling () {
-        for(Stock stock : stocks) {
-            String 이름 = stock.getName(); // 주식 이름
-            String 코드 = stock.getCode(); // 주식 코드
-            String url = "https://finance.naver.com/item/sise.naver?code=" + 코드;
 
-            try {
-                Document doc = Jsoup.connect(url).get();
+    private static String changeStockToFilePrintFormat(final Stock stock) throws IOException {
+        Document doc = Jsoup.connect(String.format(STOCK_URL_FORMAT, stock.getCode())).get();
 
-                Elements today = doc.select("div.rate_info > div.today");
-                String[] elements = today.text().split(" ");
-                String 현재가격 = "현재가격: " + elements[0];
-                String 전일대비증가가격 = "전일대비증가가격: " + elements[4] + "원 " + elements[3];
-                String 등락율 = "등락율: " + elements[7] + " " + elements[8] + "%";
-                Elements element = doc.select("div.rate_info > table.no_info > tbody > tr");
-                elements = element.text().split(" ");
+        // 현재가격, 전일대비증가가격, 등락율
+        Elements today = doc.select("div.rate_info > div.today");
+        String[] elements = today.text().split(" ");
+        String 현재가격 = "현재가격: " + elements[0];
+        String 전일대비증가가격 = "전일대비증가가격: " + elements[4] + "원 " + elements[3];
+        String 등락율 = "등락율: " + elements[7] + " " + elements[8] + "%";
 
-                String 전일종가 = elements[0] + "종가: " + elements[1];
-                String 고가 = elements[3] + ": " + elements[4].substring(0, elements[4].length()/2);
-                String 상한가 = elements[5].substring(1) + " " + elements[6].substring(0, elements[6].length()/2);
-                String 거래량 = elements[8] + ": " + elements[9];
-                String 시가 = elements[11] + ": " + elements[12].substring(0, elements[12].length()/2);
-                String 저가 = elements[13] + ": " + elements[14].substring(0, elements[14].length()/2);
-                String 하한가 = elements[15].substring(1) + ": " + elements[16];
-                String 거래대금 = elements[18] + ": " + elements[19] + " " + elements[21];
-                /*System.out.println(이름);
-                System.out.println(전일종가);
-                System.out.println(고가);
-                System.out.println(상한가);
-                System.out.println(거래량);
-                System.out.println(시가);
-                System.out.println(저가);
-                System.out.println(하한가);
-                System.out.println(거래대금);
-                */
-                String str = 이름 + "\n" +
-                        현재가격 + "\n" +
-                        전일대비증가가격 + "\n" +
-                        등락율 + "\n" +
-                        전일종가 + "\n" +
-                        시가 + "\n" +
-                        저가 + "\n" +
-                        고가 + "\n" +
-                        하한가 + "\n" +
-                        상한가 + "\n" +
-                        거래량 + "\n" +
-                        거래대금 + "\n";
-                
-                out.println(str); // 크롤링 하고 파일에 쓰기
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return result;
+        // 전일종가, 고가, 상한가, 거래량, 시가, 저가, 하한가, 거래대금
+        Elements element = doc.select("div.rate_info > table.no_info > tbody > tr");
+        elements = element.text().split(" ");
+        String 전일종가 = elements[0] + "종가: " + elements[1];
+        String 고가 = elements[3] + ": " + elements[4].substring(0, elements[4].length()/2);
+        String 상한가 = elements[5].substring(1) + " " + elements[6].substring(0, elements[6].length()/2);
+        String 거래량 = elements[8] + ": " + elements[9];
+        String 시가 = elements[11] + ": " + elements[12].substring(0, elements[12].length()/2);
+        String 저가 = elements[13] + ": " + elements[14].substring(0, elements[14].length()/2);
+        String 하한가 = elements[15].substring(1) + ": " + elements[16];
+        String 거래대금 = elements[18] + ": " + elements[19] + " " + elements[21];
+
+        return stock.getName() + "\n" +
+                현재가격 + "\n" +
+                전일대비증가가격 + "\n" +
+                등락율 + "\n" +
+                전일종가 + "\n" +
+                시가 + "\n" +
+                저가 + "\n" +
+                고가 + "\n" +
+                하한가 + "\n" +
+                상한가 + "\n" +
+                거래량 + "\n" +
+                거래대금 + "\n";
     }
 
 }
--- stockSave/untitled/src/Stock.java
+++ stockSave/untitled/src/Stock.java
@@ -1,18 +1,37 @@
-class Stock {
-    private String name;
-    private String code;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
-    Stock(String name, String code) {
+public enum Stock {
+    EV_ADVANCED_MATERIALS("이브이첨단소재", "131400"),
+    CRYSTAL_NEW_MATERIAL("크리스탈신소재 ", "900250"),
+    POSCO_M_TECH("포스코엠텍 ", "009520"),
+    EID("이아이디 ", "093230"),
+    URBAN_LITHIUM("어반리튬 ", "073570"),
+    ECO_PRO("에코프로 ", "086520"),
+    MOBASE("모베이스 ", "101330"),
+    TSI("티에스아이 ", "277880 "),
+    DONGKUK_INDUSTRY("동국산업 ", "005160");
+
+    private final String name;
+    private final String code;
+
+    Stock(final String name, final String code) {
         this.name = name;
         this.code = code;
     }
 
+    public static List<Stock> sortedValues() {
+        Stock[] values = Stock.values();
+        Arrays.sort(values, Comparator.comparing(o -> o.name));
+        return List.of(values);
+    }
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public String getCode() {
-        return code;
+        return this.code;
     }
 
     @Override
